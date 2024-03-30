8ba669f588564882726afe9fcbb18b5f73dddce1
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-30 12:09:58 -0300

    style: add dark theme and toggle theme

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,8 +1,20 @@
 'use client'
 
 import { ScheduleProvider } from '@/context/schedule-context'
+import { ThemeProvider } from 'next-themes'
 import { ReactNode } from 'react'
 
 export function Provider({ children }: { children: ReactNode }) {
-  return <ScheduleProvider>{children}</ScheduleProvider>
+  return (
+    <ScheduleProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </ScheduleProvider>
+  )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,14 @@
 import { ChangeDateButton } from './change-date-button'
+import { ToggleTheme } from './toggle-theme'
 
 export function Header() {
   return (
-    <header className="flex w-full h-28 justify-between items-center px-12">
+    <header className="flex w-full justify-between items-center pt-16 px-12">
       <h1 className="font-bold text-4xl">Shift Planner</h1>
-      <ChangeDateButton />
+      <div className="flex items-center gap-4">
+        <ChangeDateButton />
+        <ToggleTheme />
+      </div>
     </header>
   )
 }
